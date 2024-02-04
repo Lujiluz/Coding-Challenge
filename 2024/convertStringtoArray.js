067e9e072ca01e72a89b984e37979aab65c83b50
Luji <zlfjrii@gmail.com>
2024-02-04 22:38:08 +0700

    FInished #12 challenge: Convert string to array

---
+++ 2024/convertStringtoArray.js
@@ -0,0 +1,8 @@
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+// Done by me🫡
+
+console.log(stringToArray('Robin Singh'));
+console.log(stringToArray('I love arrays they are my favorite'));
