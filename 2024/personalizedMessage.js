d88f6dbf6bc2d441be92e76aaa12d6c3dbb18adf
Luji <zlfjrii@gmail.com>
2024-02-22 16:05:20 +0700

    Finished #19 challenge: Personalized Message

---
+++ 2024/personalizedMessage.js
@@ -0,0 +1,8 @@
+function greet(name, owner) {
+  return name === owner ? `Hello boss` : `Hello guest`;
+}
+
+// done by me🫡
+
+console.log(greet('Daniel', 'Daniel'));
+console.log(greet('Greg', 'Daniel'));
